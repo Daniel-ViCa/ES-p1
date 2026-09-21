a4c8314271581426aff6bb8f431a48fe89ffc346
aula <u1994973@campus.udg.edu>
2026-09-21 19:08:29 +0200

    commit inicial

---
+++ practica-1-master/src/test/java/org/udg/caes/banking/entity/TestCreditCard.java
@@ -0,0 +1,19 @@
+package org.udg.caes.banking.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+//test de la funcio credit
+
+
+
+public class TestCreditCard {   //Entrem dins la classe de la funcio
+    @Test
+    void testCredit() {         //Generem la funcio del metode que volem fer el testing
+        CreditCard cc = new CreditCard("test");     //Arrange (preparar)
+        cc.credit(100);                         //Act (Executem - actuem el metode)
+        assertEquals(100, cc.getCredit());      //Assert (verifiquem els resultats)
+    }
+}
